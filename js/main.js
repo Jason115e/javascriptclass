d10732a9e47efb750081da494bd922eca178b132
Jason <cardonajason1013@gmail.com>
2019-08-12 22:47:18 -0700

    Learned Buttons and basic functions with onclick - Class 1

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function myfunk() {
+
+let question = prompt('Tell me your name.');
+console.log('your name is ' + question);
+
+  }
+
+
+
+function popup(){
+  console.log('Fishing');
+  console.log('Gamer');
+  console.log('Something Else');
+  console.log('First Person Shooter');
+  console.log('Something');
+
+
+}
