d604d5079673e1e2b4691ecc2570a33b61613d88
Jason <cardonajason1013@gmail.com>
2019-08-14 19:52:44 -0700

    End of Day 2

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
-var firstName = "Jason";
-var age = 31;
+// var firstName = "Jason";
+// var age = 31;
+//
+// console.log("My name is " + firstName + " and my age is " + age );
+//
+// //better way
+// console.log(`My name is ${firstName} and my age is ${age}`);
+// //alert(`Hello my name is ${firstName}`);
+//
+// const birthday = '09/05/1987';
+//
+// let fixDate = new Date(birthday);
+//
+// let hello = new String("Hello, There");
+// alert(hello);
 
-console.log("My name is " + firstName + " and my age is " + age );
+//
+// const topThreeMovies = ['Matrix', ' Matrix2', ' Matrix 3'];
+//
+// console.log(`My top three movies are ${topThreeMovies}`);
+//
+// console.log(`My top favorite movie is ${topThreeMovies[0]}`);
 
-//better way
-console.log(`My name is ${firstName} and my age is ${age}`);
-//alert(`Hello my name is ${firstName}`);
 
-const birthday = '09/05/1987';
-
-let fixDate = new Date(birthday);
-
-let hello = new String("Hello, There");
-alert(hello);
+prompt('What is your first Name?:');
