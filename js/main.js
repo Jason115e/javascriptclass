1532b5966bdd22f2143586e909245d7415e98e11
Jason <cardonajason1013@gmail.com>
2019-08-14 19:04:44 -0700

    Day 2 Better way of using string and variables in a much shorter way
    using date

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var firstName = "Jason";
+var age = 31;
+
+console.log("My name is " + firstName + " and my age is " + age );
+
+//better way
+console.log(`My name is ${firstName} and my age is ${age}`);
+//alert(`Hello my name is ${firstName}`);
+
+const birthday = '09/05/1987';
+
+let fixDate = new Date(birthday);
+
+let hello = new String("Hello, There");
+alert(hello);
