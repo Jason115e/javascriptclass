4734cbd365b076b7642eb8170575c9ad90db0fa2
Jason <cardonajason1013@gmail.com>
2019-08-20 12:41:08 -0700

    Final Day 3 Take home

--- js/main.js
+++ js/main.js
@@ -1,78 +1,72 @@
-const userName = prompt("What is your name ?: ");
-const favColor = prompt("What is your favorite color: Red, Orange, Yellow, Green, Blue, Pink, Purple, Black, White, Grey ").toUpperCase();
+const userName = prompt("What is your name : ");
+const favColor = prompt("What is your favorite color: Red, Orange, Yellow, Green, Blue, Pink, Purple, Black, White, Grey : ").toUpperCase();
 const H1 = document.getElementById("msg");
-
-console.log(favColor);
 
 switch (favColor) {
   case 'RED':
 
-console.log(`First name is: ${fname}`);
-alert(`Your last name is ${lname}`);
-
-    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+    H1.innerHTML = (`${userName}, You have selected the following color: ${favColor.toLowerCase()}`);
  
     break;
 
   case 'ORANGE':
 
-  alert(`Your Birthday is on ${birthday}`);
 
-    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+    H1.innerHTML = (`${userName}, You have selected the following color: ${favColor.toLowerCase()}`);
 
     break;
 
   case 'YELLOW':
 
 
-    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+    H1.innerHTML = (`${userName}, You have selected the following color: ${favColor.toLowerCase()}`);
 
     break;
 
   case 'GREEN':
 
 
-    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+    H1.innerHTML = (`${userName}, You have selected the following color: ${favColor.toLowerCase()}`);
 
     break;
 
   case 'BLUE':
 
 
-    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+    H1.innerHTML = (`${userName}, You have selected the following color: ${favColor.toLowerCase()}`);
 
     break;
 
   case 'PINK':
 
 
-    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+    H1.innerHTML = (`${userName}, You have selected the following color: ${favColor.toLowerCase()}`);
 
     break;
 
   case 'PURPLE':
 
 
-    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+    H1.innerHTML = (`${userName}, You have selected the following color: ${favColor.toLowerCase()}`);
 
     break;
 
   case 'BLACK':
 
 
-    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+    H1.innerHTML = (`${userName}, You have selected the following color: ${favColor.toLowerCase()}`);
 
     break;
   case 'WHITE':
 
 
-    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+    H1.innerHTML = (`${userName}, You have selected the following color: ${favColor.toLowerCase()}`);
 
     break;
   case 'GREY':
 
 
-    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+    H1.innerHTML = (`${userName}, You have selected the following color: ${favColor.toLowerCase()}`);
 
     break;
 
