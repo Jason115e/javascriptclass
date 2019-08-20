31144b13048ed4481f0a8c2370e84697b1fe0b23
Jason <cardonajason1013@gmail.com>
2019-08-20 08:20:39 -0700

    Finished Take home day 3

--- js/main.js
+++ js/main.js
@@ -1,18 +1,80 @@
-function myfunk() {
+const userName = prompt("What is your name ?: ");
+const favColor = prompt("What is your favorite color: Red, Orange, Yellow, Green, Blue, Pink, Purple, Black, White, Grey ").toUpperCase();
+const H1 = document.getElementById("msg");
 
-let question = prompt('Tell me your name.');
-console.log('your name is ' + question);
+console.log(favColor);
 
-  }
+switch (favColor) {
+  case 'RED':
 
 
+    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+ 
+    break;
 
-function popup(){
-  console.log('Fishing');
-  console.log('Gamer');
-  console.log('Something Else');
-  console.log('First Person Shooter');
-  console.log('Something');
+  case 'ORANGE':
 
+
+    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+
+    break;
+
+  case 'YELLOW':
+
+
+    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+
+    break;
+
+  case 'GREEN':
+
+
+    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+
+    break;
+
+  case 'BLUE':
+
+
+    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+
+    break;
+
+  case 'PINK':
+
+
+    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+
+    break;
+
+  case 'PURPLE':
+
+
+    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+
+    break;
+
+  case 'BLACK':
+
+
+    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+
+    break;
+  case 'WHITE':
+
+
+    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+
+    break;
+  case 'GREY':
+
+
+    H1.innerHTML = (`${userName}, You Choosed the color ${favColor.toLowerCase()}`);
+
+    break;
+
+
+  default:
+    console.log(`Sorry ${userName}, that is an invalid color`);
 
 }
