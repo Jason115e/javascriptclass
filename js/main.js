680b8bbbdc84a1f50a6483c9dee40ccd888a9663
Jason <cardonajason1013@gmail.com>
2019-08-21 22:42:16 -0700

    Fun with arrays

---
+++ js/main.js
@@ -0,0 +1,166 @@
+// const greeting = 1;
+
+// if (greeting == "Hi") {
+// console.log("Yes, this matches to Hi");
+
+// } else if (greeting == "hello"){
+//     console.log("Yes, this matches");
+
+
+// } else if(greeting == 1){
+// console.log("Yes, this matches an integer of one")
+
+// }
+
+// else{
+
+//     console.log("None, of them matches your selection");
+// }
+
+
+
+// function lowerCasess(v){
+
+//  return v.toLowerCase();
+
+
+// }
+
+// console.log(lowerCasess ("HELLO"));
+
+
+// function getWeatherReport(day, time){
+
+// const
+
+// }
+
+
+// let myArrayRandomInfo = ["TheMatrix", 13, 0, 2005];
+
+// console.log(myArrayRandomInfo.length);
+// console.log(myArrayRandomInfo[1]);
+// console.log(Array.isArray(myArrayRandomInfo));
+// myArrayRandomInfo.push('Another array');
+// console.log(myArrayRandomInfo.length);
+// console.log(myArrayRandomInfo);
+// myArrayRandomInfo.unshift('Starting Array');
+// console.log(myArrayRandomInfo);
+// console.log(myArrayRandomInfo[0]);
+// myArrayRandomInfo.pop();
+// console.log(myArrayRandomInfo);
+// myArrayRandomInfo.shift();
+// console.log(myArrayRandomInfo);
+// myArrayRandomInfo.splice(1,1,5); //first param is position, 2nd = How many you want to replace after //
+// console.log(myArrayRandomInfo);
+// let newArrayOfInfo =  myArrayRandomInfo.slice(1);
+// console.log(newArrayOfInfo);
+
+// newArrayOfInfo.indexOf('TheMatrix');
+// console.log(newArrayOfInfo.indexOf(false));
+
+
+// myArrayRandomInfo.reverse();
+// console.log(newArrayOfInfo);
+
+
+// let arrayOne = [
+//     'The Matrix', 'Neo'
+// ]
+// let arrayTwo = [
+//     'Batman', 'Bruce Wayne'
+// ]
+// let arrayThree = [
+//     'Lion King', 'Simba'
+// ]
+
+// let arrayFour = [
+//     'Titanic', 'Rose'
+// ]
+
+// let arrayFive = [
+//     'Moana', 'The Rock'
+// ]
+
+// let favoriteMovies = [
+//     arrayOne[0], 
+//     arrayTwo[0], 
+//     arrayThree[0], 
+//     arrayFour[0], 
+//     arrayFive[0]
+
+
+// ]
+
+// alert(favoriteMovies.indexOf[0];
+
+
+    //Loops understanding
+
+    let x = 0;
+
+    while (x <= 10){
+
+        console.log(x);
+        x++;
+    }
+
+
+    //for loop
+
+    for (let i = 0; i < 20; i++){
+        console.log(i);
+    }
+
+
+    // for (let i = 10; i < 0; i--){
+    //     console.log(i);
+    // }
+
+
+//  let i = 0;
+
+//  do{
+// console.log(i);
+// i++
+
+//  }while (i < 10);
+
+
+
+ //for each loop
+
+ 
+
+// let array3 = [1,2,3,4,5];
+
+// array3.forEach(function(x){
+// console.log(x);
+
+
+// })
+
+
+// let myString = "My name is Jason";
+// for (i of myString){
+// console.log(i);
+
+// }
+
+
+//for end
+
+
+let people = {
+
+firstName: 'Jason',
+LastName: 'Cardona'
+
+}
+
+
+
+
+for (i in people){
+    console.log(people[i]);
+}
