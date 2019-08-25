5fa650bbde8ad2a166e835ad842ff5b46153771d
Jason <cardonajason1013@gmail.com>
2019-08-25 13:23:48 -0700

    Added a random function to play with

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const procees1 = document.getElementById('procees');
+const questionasked = prompt("What is your name").toLocaleUpperCase();
+
+
+
+procees1.addEventListener('click', function(e){
+
+if(!questionasked == NaN){
+    
+}
+
+
+});
+
+
+
+
+
+
