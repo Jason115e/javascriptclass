63d5549ea1a5a3e591d83bde95a8cd63c39bbe90
Jason <cardonajason1013@gmail.com>
2019-08-26 19:34:49 -0700

    Fun with funcstions

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// const task1 = prompt("Give me 1st task to do");
+// const task2 = prompt("Give me a 2nd task to do");
+// const task3 = prompt("Give me 3rd task to do");
+
+// let arrayOfTasks = [task1, task2, task3];
+
+
+
+// console.log(arrayOfTasks);
+
+// const  = 3;
+// for(j = 0; j < limit; j++){
+
+//     arrayOfTasks[j].push(`This will take ${Math.floor(Math.random() * 15)}`);
+//     console.log(arrayOfTasks);
+// };
+
+
+// let example  = function (){
+
+//     console.log("Hello There")
+// }
+
+// function example(){
+
+
+// }
+
+
+
+
+// let multiply = (arg1, arg2) => {return arg1 * arg2};
+
+// multiply(2,4);
+
+// let x = multiply(2,4);
+// console.log(x)
+
+
+
