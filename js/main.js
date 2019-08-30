7969e5ee5be49c06cb5d60e22857560797560a88
Jason <cardonajason1013@gmail.com>
2019-08-29 20:54:53 -0700

    Start of a fah/cel cnverter

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const userInputDegree = document.getElementById("degreeInput");
+const userDegreeOutput = document.getElementById("degreeOutput");
+
+
+function changetoFahrenheit (degree, scale) {
+
+
+    let x = (degree - 32)*5/6;
+    console.log(x);
+
+
+
+
+
+}
