c0f91a01985b9894a2f2c8c001509a018bb59619
Jason <cardonajason1013@gmail.com>
2019-08-30 19:37:40 -0700

    Added a functional degree converter, need to add a function to do the converting

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
 const userInputDegree = document.getElementById("degreeInput");
 const userDegreeOutput = document.getElementById("degreeOutput");
-
-
-function changetoFahrenheit (degree, scale) {
-
-
-    let x = (degree - 32)*5/6;
-    console.log(x);
+const buttonFahrenheit = document.getElementById("selectionFahrenheit");
+const buttonCelcius = document.getElementById("selectionCelcius");
+const errorMessage = document.getElementById('errormessage');
 
 
 
+buttonFahrenheit.addEventListener('click', () => {
+    let numberisNumber = isNaN(userInputDegree.value);
+    if (!numberisNumber) {
+        console.log("is a number");
+        userDegreeOutput.value = `Celcius: ${parseFloat((userInputDegree.value - 32) * 5/9).toFixed(2)} `;
+        errorMessage.innerHTML = "";
+    } else {
+        console.log("Is not a number");
+        errorMessage.innerHTML = "Please enter an actual number!";
+    }
+});
+buttonCelcius.addEventListener('click', () => {
+    let numberisNumber = isNaN(userInputDegree.value);
+    if (!numberisNumber) {
+        console.log("is a number");
+        userDegreeOutput.value = `Fahrenheit: ${parseFloat((userInputDegree.value * 9/5) + 32).toFixed(2)} `;
+        errorMessage.innerHTML = "";
+    } else {
+        console.log("Is not a number");
+        errorMessage.innerHTML = "Please enter an actual number!";
+    }
+});
 
-
-}
