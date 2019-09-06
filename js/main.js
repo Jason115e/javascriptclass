efbdcea2ae1495f1de8980b26f913fe3d8aafd4b
Jason <cardonajason1013@gmail.com>
2019-09-06 07:46:21 -0700

    Finished take home day 8 with 2 functino and 1 arrow function

--- js/main.js
+++ js/main.js
@@ -1,52 +1,30 @@
 
-
-
 function superAwesomeFunction() {
-   
-  
-    
-    
-
     formCreation();
     labelCreation();
-
     let removeItem = document.getElementById('startButton');
     removeItem.classList.add('hidden');
-
-
 }
 //<input type="" name="" class="" placeholder="" / elementInnerText for Button>
 function formCreation() {
     let container = document.getElementById('container');
     let form = inputCreation('form', '', 'formName', 'GET', 'js/formSubmission.js', 'inputform', '', '');
-    let inputName = inputCreation('input', 'name', 'name', '', '', 'form-control', '', '');
+    let inputName = inputCreation('input', 'text', 'name', '', '', 'form-control', '', '');
     let inputEmail = inputCreation('input', 'email', 'email', '', '', 'form-control', '', '');
     let textArea = inputCreation('textarea', 'text', 'textarea', '', '', 'form-control', '', '');
     let button = inputCreation('button', 'submit', 'submit', '', '', '', '', 'Submit');
     let formGroupName = inputCreation('div', '', '', '', '', 'form-group', '', '');
     let formGroupEmail = inputCreation('div', '', '', '', '', 'form-group', '', '');
-    
 
     container.appendChild(form);
-
     form.appendChild(formGroupName);
-    
     formGroupName.appendChild(labelCreation('name', 'Name:'));
     formGroupName.appendChild(inputName);
-
-
     form.appendChild(formGroupEmail);
-    
     formGroupEmail.appendChild(labelCreation('email', 'Email:'));
     formGroupEmail.appendChild(inputEmail);
-
-
     formGroupEmail.appendChild(textArea);
-
     form.appendChild(button);
-
-
-    
 }
 //<label for=""></label>
 function labelCreation(elementName, elementInfo) {
@@ -56,7 +34,8 @@ function labelCreation(elementName, elementInfo) {
     return label;
 }
 
-function inputCreation(elementTag, elemetType, elementName, elementMethod = '', elementAction, elementClass, elementPlaceholder, elementInnerText = '') {
+
+const inputCreation = (elementTag, elemetType, elementName, elementMethod = '', elementAction, elementClass, elementPlaceholder, elementInnerText = '') => {
     const elementInput = document.createElement(elementTag);
     elementInput.setAttribute('type', elemetType);
     elementInput.setAttribute('name', elementName);
