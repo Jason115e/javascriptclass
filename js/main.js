4b40bbd62788767e9d0f86ba1ca9f600cd881e26
Jason <cardonajason1013@gmail.com>
2019-09-18 18:56:14 -0700

    Finished Final

--- js/main.js
+++ js/main.js
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-
-function superAwesomeFunction() {
-    formCreation();
-    labelCreation();
-    let removeItem = document.getElementById('startButton');
-    removeItem.classList.add('hidden');
-}
-//<input type="" name="" class="" placeholder="" / elementInnerText for Button>
-function formCreation() {
-    let container = document.getElementById('container');
-    let form = inputCreation('form', '', 'formName', 'GET', 'js/form-submission.js', 'inputform', '', '');
-    let inputName = inputCreation('input', 'text', 'name', '', '', 'form-control', '', '');
-    let inputEmail = inputCreation('input', 'email', 'email', '', '', 'form-control', '', '');
-    let textArea = inputCreation('textarea', 'text', 'textarea', '', '', 'form-control', '', '');
-    let button = inputCreation('button', 'submit', 'submit', '', '', '', '', 'Submit');
-    let formGroupName = inputCreation('div', '', '', '', '', 'form-group', '', '');
-    let formGroupEmail = inputCreation('div', '', '', '', '', 'form-group', '', '');
-
-    container.appendChild(form);
-    form.appendChild(formGroupName);
-    formGroupName.appendChild(labelCreation('name', 'Name:'));
-    formGroupName.appendChild(inputName);
-    form.appendChild(formGroupEmail);
-    formGroupEmail.appendChild(labelCreation('email', 'Email:'));
-    formGroupEmail.appendChild(inputEmail);
-    formGroupEmail.appendChild(textArea);
-    form.appendChild(button);
-}
-//<label for=""></label>
-function labelCreation(elementName, elementInfo) {
-    let label = document.createElement('label');
-    label.setAttribute('for', elementName);
-    label.innerText = elementInfo;
-    return label;
-}
-
-
-const inputCreation = (elementTag, elemetType, elementName, elementMethod = '', elementAction, elementClass, elementPlaceholder, elementInnerText = '') => {
-    const elementInput = document.createElement(elementTag);
-    elementInput.setAttribute('type', elemetType);
-    elementInput.setAttribute('name', elementName);
-    elementInput.setAttribute('method', elementMethod);
-    elementInput.setAttribute('action', elementAction);
-    elementInput.setAttribute('class', elementClass);
-    elementInput.setAttribute('placeholder', elementPlaceholder);
-    elementInput.innerText = elementInnerText;
-    return elementInput;
-}
-=======
 let button = document.createElement("button");
 let cancelbutton = document.createElement("button");
 let body = document.getElementById("main");
@@ -167,4 +117,3 @@ function slide3Question(){
 
 
 }
->>>>>>> final
